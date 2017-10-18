856a0144bedb2d12daa63a9b2ab45c20fcc37143
sumanbhushal <sumanbhushal@gmail.com>
2017-10-18 17:15:16 +0200

    opinion extraction and summary in progress

--- opinion_extraction.py
+++ opinion_extraction.py
@@ -3,7 +3,6 @@ from nltk.corpus import wordnet
 from nltk.corpus import sentiwordnet
 
 
-
 def extract_opinon_of_feature(pos_tagged_sentence_list):
     features_list = ['adjustment']
     for features in features_list:
@@ -42,7 +41,8 @@ def extract_opinon_of_feature(pos_tagged_sentence_list):
                     opinions.append(min(distance_btw_feature_opinion, key=distance_btw_feature_opinion.get))
                     #print(min(distance_btw_feature_opinion, key=distance_btw_feature_opinion.get))
         print(opinions)
-        word_orientation(opinions)
+        # word_orientation(opinions)
+        generate_summary(features, len(sentences_with_feature), opinions)
 
 
 def extract_opinion(pos_tagged_sentence_list):
@@ -60,21 +60,49 @@ def extract_opinion(pos_tagged_sentence_list):
 
 # WordNet to get the syns set and use SentiWordNet to find the orientation of the word
 def word_orientation(input_word):
+    opinion_orientation_of_feature = 0
     for word in input_word:
-        word_for_orientation = word + '.a.01'
-        each_word_orientation = sentiwordnet.senti_synset(word_for_orientation)
-        print(word_for_orientation, each_word_orientation)
+        syns = wordnet.synsets(word)
+        if (len(syns) != 0):
+            word = syns[0].name()
+            each_word_orientation = sentiwordnet.senti_synset(word)
+            print(word, each_word_orientation)
+            if each_word_orientation.pos_score() != 0:
+                opinion_orientation_of_feature += (each_word_orientation.pos_score() - each_word_orientation.neg_score())
+            elif each_word_orientation.neg_score() != 0:
+                opinion_orientation_of_feature -= each_word_orientation.neg_score()
+                print(opinion_orientation_of_feature)
+    print(" Totatl Orientation", opinion_orientation_of_feature)
+    normalized_value = (opinion_orientation_of_feature) / len(input_word)
+    print(" Totatl Orientation", normalized_value)
+
+        # word_for_orientation = word + '.a.01'
+        # each_word_orientation = sentiwordnet.senti_synset(word_for_orientation)
+        # print(word_for_orientation, each_word_orientation)
         # syns = wordnet.synsets(word)
-        # print(syns[0].name())
         # for i in syns:
         #     if i.pos() in ['a', 's']:
-        #         print(word, i.lemmas()[0].name())
-                # for j in i.lemmas():
-                #     print(word, j[0].name())
-        # if (len(syns) != 0):
-        #     word = syns[0].name()
-        #     word_orientation = sentiwordnet.senti_synset(word)
-        #     print(word_orientation)
-        #     print(word_orientation.pos_score())
-    #print(syns[0])
+        #         print(i.lemmas())
 
+
+def generate_summary(feature, sentence_count, input_word):
+    opinion_orientation_of_feature = 0
+    pos, neg, neu = 0, 0, 0
+    for word in input_word:
+        syns = wordnet.synsets(word)
+        if (len(syns) != 0):
+            word = syns[0].name()
+            each_word_orientation = sentiwordnet.senti_synset(word)
+            if each_word_orientation.pos_score() != 0:
+                if each_word_orientation.pos_score() - each_word_orientation.neg_score() > 0:
+                    pos += 1
+                else:
+                    neg += 1
+            elif each_word_orientation.neg_score() != 0:
+                # opinion_orientation_of_feature -= each_word_orientation.neg_score()
+                neg += 1
+            else:
+                neu += 1
+
+    print("Feature = ", feature)
+    print("total posts =", sentence_count, "\n" "positives =", pos, ", negatives =", neg, ", neutrals =", neu)
